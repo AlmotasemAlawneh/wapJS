67fdf3a0086a86b56cd5126632f5ed471384b73f
unkown <610137@CS.MUM.EDU>
2019-07-03 18:30:37 -0500

    updated with module implementation

--- waplab03/lab03.js
+++ waplab03/lab03.js
@@ -1,20 +1,44 @@
-var fName; 
+const Accounts = (function() {
+    //const accoountsArray = [];
+    const accountCreator = function(inName, inAmt){
+        // accoountsArray.push(
+            accountInfoList
+            return {
+                name:inName,
+                amount:inAmt
+            };
+        // );
+    };
+    return{
+        accountCreate:accountCreator
+        //,getAccounts: function getAll() { return accoountsArray;} 
+    };
+}
+)();
+
 window.onload  =() => {
+    accountInfoList = [];
     fName = document.getElementById("fname") ; 
     aType= document.getElementById("accountType");
     req = document.getElementById("request"); 
     paddableTag = document.getElementsByClassName("paddable"); // returns an array
     accountsTextArea = document.getElementById("accountsTA");
     depAmt = document.getElementById("depositeAmt");
+    
 };
 
 function logAll() {
-    accountsTextArea.value += (fName.value+ ' ' + aType.value + ' ' + depAmt.value +' deposite \n')    
-    console.log(req.textContent);
+    accountsTextArea.value += (fName.value+ ' ' + aType.value + ' ' + depAmt.value +' deposit \n')    
+    var account = Accounts.accountCreate(fName.value, depAmt.value);
+    accountInfoList.push(account);
+    // console.log(Accounts.getAccounts());
+    // console.log(accountInfoList); 
 }
 
+
+
 function updateRequest(){
-    req.textContent = fName.value + ' wants to create a ' + aType.value +' account with deposite ' + depAmt.value;
+    // req.textContent = fName.value + ' wants to create a ' + aType.value +' account with deposite ' + depAmt.value;
 }
 
 function increasePadding(){
