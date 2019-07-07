7058c5c332e7c0155002c771bc7705973f64cce1
unkown <610137@CS.MUM.EDU>
2019-07-06 19:13:13 -0500

    now the puzzle pieces are not ontop of each other (flex wrap aqnd position relative)

--- waplab05/cheerleader.js
+++ waplab05/cheerleader.js
@@ -5,10 +5,10 @@ $(function() {
 function cheer(e) {
     $("<li>")
         .text(String.fromCharCode(e.which).toUpperCase() + "!")
-        .appendTo(REPLACE THIS WITH APPROPRIATE CODE);
-    //setTimeout(removeCheer, 2000);
+        .appendTo('#cheers');
+    setTimeout(removeCheer, 2000);
 }
 
 function removeCheer() {
-    $(REPLACE THIS WITH APPROPRIATE CODE).first().remove();
+    $("#cheers li").first().remove();
 }
